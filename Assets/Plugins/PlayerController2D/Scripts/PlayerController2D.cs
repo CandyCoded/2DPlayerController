6f3026fdaaa945e74ae63ba404dc05449f2f49c6
Scott Doxey <neogeek0101@gmail.com>
2019-03-14 19:26:28 -0400

    Removed unused imports.

--- Assets/Plugins/PlayerController2D/Scripts/PlayerController2D.cs
+++ Assets/Plugins/PlayerController2D/Scripts/PlayerController2D.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
-using CandyCoded;
 using UnityEngine.Events;
 
 namespace CandyCoded.PlayerController2D
